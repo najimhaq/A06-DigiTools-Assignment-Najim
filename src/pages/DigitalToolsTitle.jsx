a74af5120f6a96526db7ba66f82cbadbe1ac3da6
najimhaq <mdnajimulhaque@gmail.com>
2026-03-31 19:49:10 +0900

    h and p correction

--- src/pages/DigitalToolsTitle.jsx
+++ src/pages/DigitalToolsTitle.jsx
@@ -5,14 +5,14 @@ const DigitalToolsTitle = () => {
     <>
       {/* Heading */}
       <div className='text-center mb-10'>
-        <h1 className='font-black text-5xl mb-4 tracking-tight text-[#101727]'>
+        <h1 className='font-black text-5xl md:text-6xl mb-6 tracking-tight text-gray-900'>
           Premium Digital Tools
         </h1>
-        <p className='text-md text-[#627382]'>
+        <p className='text-xl text-gray-600 max-w-2xl mx-auto'>
           Choose from our curated collection of premium digital products
           designed
         </p>
-        <p className='text-md text-[#627382]'>
+        <p className='text-xl text-gray-600 max-w-2xl mx-auto'>
           to boost your productivity and creativity.
         </p>
       </div>
--- src/pricingCard/PricingCard.jsx
+++ src/pricingCard/PricingCard.jsx
@@ -5,14 +5,14 @@ const PricingCard = () => {
   return (
     <>
       <div className='text-center mb-10'>
-        <h1 className='font-black text-5xl mb-4 tracking-tight text-[#101727]'>
+        <h1 className='font-black text-5xl md:text-6xl mb-6 tracking-tight text-gray-900'>
           Simple, Transparent Pricing
         </h1>
-        <p className='text-md text-[#627382]'>
+        <p className='text-xl text-gray-600 max-w-2xl mx-auto'>
           Choose the plan that fits your needs. Upgrade or downgrade anytime.
         </p>
       </div>
-      <PlanPrice/>
+      <PlanPrice />
     </>
   );
 };
